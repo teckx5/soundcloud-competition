c6f107ec42cc7849bdf713d6042abe7059d38725
Lee Martin <leepaulmartin@gmail.com>
2011-07-24 08:52:04 -0700

    pg fix

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -3,10 +3,10 @@ class Competition < ActiveRecord::Base
   has_many :tracks
 
   def latest_tracks
-    self.tracks.all(:limit => 3, :order => "created_at DESC")
+    Track.all(:limit => 3, :order => "created_at DESC")
   end
 
   def top_tracks
-    self.tracks.rank_tally(:limit => 3)
+    Track.rank_tally(:limit => 3)
   end
 end
