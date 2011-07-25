1a53fce61780ec40e828034d6f7b62e6c82a114c
Lee Martin <leepaulmartin@gmail.com>
2011-07-25 16:03:02 -0700

    cleaned up seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,9 @@ data = {
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat leo vitae leo rutrum ullamcorper. Maecenas in orci nulla. Aenean gravida adipiscing arcu at sodales. Donec nisl lacus, gravida sit amet aliquam vitae, pulvinar at sapien. Nullam congue convallis libero, ut luctus erat posuere eu. Fusce sit amet ante nisi, ac viverra ligula. Etiam vel velit tellus. Phasellus pharetra, ligula fermentum interdum lobortis, lorem arcu pulvinar urna, vel blandit augue augue pulvinar elit. Duis nec justo ut mi sollicitudin eleifend sed sit amet leo. Quisque aliquet quam eget tortor tincidunt malesuada. Vivamus at neque ut sem molestie vulputate ac id diam. Fusce ut pulvinar leo. Curabitur sit amet dui ut nulla venenatis pretium ac vel dolor. Aliquam id imperdiet arcu. Vestibulum molestie venenatis tellus, sed lobortis nisi sodales ac.',
   prizes: '* Prize 1',
   about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat leo vitae leo rutrum ullamcorper. Maecenas in orci nulla. Aenean gravida adipiscing arcu at sodales.',
-  group: 'http://soundcloud.com/groups/competition',
-  key: 'J8aFr3h5xyOSkYxsJMYXQ',
+  #group: 0,
+  #rules: '',
+  #download: '',
   start_date: Time.now,
   end_date: Time.now + 30.days
 }
